67bf02fb81df08d99fb7e9444ca23e0a18de4cb2
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-07-01 02:21:03 +0700

    Initial Commit

---
+++ inoutExample/inoutExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  inoutExampleApp.swift
+//  inoutExample
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct inoutExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
